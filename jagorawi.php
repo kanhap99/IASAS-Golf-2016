89fb07f495868a138586fa3a474faefe631cc730
Kanha Prasad <41562@jisedu.or.id>
2016-03-29 15:09:27 +0700

    Jagorawi file added

---
+++ jagorawi.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <?php include 'includes/head.html' ?>
+
+    <title>Schedule | IASAS Golf 2016</title>
+
+</head>
+<style>
+    body {
+        background:url(img/backgrounds/JIS_blue.png) repeat;
+    }
+    .row{
+      padding-top: 40px;
+    }
+    td, th {
+        text-align: center;
+        padding-left:35px;
+        padding-top:10px;
+        padding-bottom:10px
+    }
+    a:visited, a:hover {
+      text-decoration: none !important;
+    }
+</style>
+<body>
+
+    <?php include 'includes/nav.html' ?>
+
+    <div class="container" style="background-color: white">
+
+      <div class="row">
+              <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12" style="padding-left:50px;">
+                  <h1 class="page-header"><a href="http://www.jagorawi.com/home.phtml" target="_blank">Jagorawi Golf Course</a></h1>
+              </div>
+      </div>
+
+    </div><!-- /.container -->
+
+    <div class="container" style="background-color: white">
+      <!--Videos-->
+      <div class="row">
+        <div class="container" style="margin: auto; text-align:center">
+          <h3>Course Hole Videos</h3>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/lwoDTHC1PZQ?list=PL6Kks4jcodhLZfxyfeH1Yt2PQUYo5IBIO" frameborder="0" allowfullscreen></iframe>
+        </div>
+      </div>
+      <!--Google map location-->
+      <div class="row">
+        <div class="container" style="margin: auto; text-align:center">
+          <h3>Location Map</h3>
+          <iframe width="600" height="450" frameborder="0" style="border:0"
+src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJxcnz0YTqaS4R6p2FayR06TY&key=AIzaSyDaGMM2Yhuk3-XRuMG4kv8tHCkofAw8aIY" allowfullscreen></iframe>
+        </div>
+      </div>
+    </div>
+
+    <?php include 'includes/footer.html' ?>
+
+</body>
+</html>
